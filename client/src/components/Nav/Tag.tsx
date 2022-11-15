a44ca0ec53b82ad11534c30355e932b448132f5c
heoputer <hjw1753@gmail.com>
2022-11-16 00:38:40 +0900

    ✨feat : 태그 (임시)

---
+++ client/src/components/Nav/Tag.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Tag = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center w-12 h-5 bg-point mx-2 text-white rounded-2xl text-tag font-semibold hover:bg-violet-600">
+        제주
+      </div>
+    </>
+  );
+};
+
+export default Tag;
