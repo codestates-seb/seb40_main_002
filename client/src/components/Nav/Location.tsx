0954f14a7830e9d618f72d06a52fdc1eefccc87f
heoputer <hjw1753@gmail.com>
2022-11-16 00:38:05 +0900

    ✨feat : 지역 선택 모달 (임시)

---
+++ client/src/components/Nav/Location.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Tag from './Tag';
+
+const Location = () => {
+  return (
+    <>
+      <div className="flex flex-wrap justify-center items-center w-selectModal h-selectModal border-solid border-2 border-borderline rounded-2xl py-1">
+        <Tag />
+        <Tag />
+        <Tag />
+        <Tag />
+        <Tag />
+        <Tag />
+        <Tag />
+        <Tag />
+        <Tag />
+        <Tag />
+      </div>
+    </>
+  );
+};
+
+export default Location;
