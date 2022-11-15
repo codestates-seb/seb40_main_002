3f39084970a664d4bd4a964c575b433ce36ab571
heoputer <hjw1753@gmail.com>
2022-11-16 00:24:10 +0900

    💄 style : Add tailwind custom styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
   content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        search: '750px',
+        selectModal: '340px',
+      },
+      height: {
+        search: '50px',
+        nav: '88px',
+        selectModal: '70px',
+      },
+      colors: {
+        point: '#629ECD',
+        borderline: '#DDDDDD',
+      },
+      borderRadius: {
+        search: '30px',
+      },
+      fontSize: {
+        tag: '14px',
+      },
+    },
   },
   variants: {
     extend: {},
