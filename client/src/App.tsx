1b289dd922f8fd5472daa5958aac77eac1435cd3
billy5982 <kuu0404@naver.com>
2022-11-15 21:30:47 +0900

    ✨ feat : 기초 세팅 완료

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,41 +1,7 @@
-import React, { useState, FormEvent } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { ReducerType } from './store/reducer/rootReducer';
-import { User, addUser } from './store/reducer/users';
+import React from 'react';
 
 export default function App() {
-  const users = useSelector<ReducerType, User[]>((state) => state.users);
-  const dispatch = useDispatch();
-
-  const [name, setName] = useState('');
-
-  const handleChangeName = (e: any) => {
-    setName(e.target.value);
-  };
-
-  const handleAddUser = (e: FormEvent) => {
-    e.preventDefault();
-    dispatch(addUser({ name } as User));
-    setName('');
-  };
-
-  return (
-    <div>
-      <form onSubmit={handleAddUser}>
-        <input type="text" value={name} onChange={handleChangeName} />
-        <button
-          type="submit"
-          className=" text-point-color bg-[#333333] rounded-lg"
-        >
-          Add User
-        </button>
-      </form>
-
-      {users.map((user) => (
-        <div key={user.id}>{user.name}</div>
-      ))}
-    </div>
-  );
+  return <div>Hello World</div>;
 }
 
 // import React from 'react';
