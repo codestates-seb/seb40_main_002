1eb5f018acfb56485ea2d83b0511e3406ac0d2db
heoputer <hjw1753@gmail.com>
2022-11-16 00:57:56 +0900

    🚚 : 파일 구조에 맞춘 경로 변경

---
+++ client/src/components/common/Nav/UserIcon.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FaUserCircle } from 'react-icons/fa';
+
+const UserIcon = () => {
+  return (
+    <>
+      <div>
+        <FaUserCircle size="50" />
+      </div>
+    </>
+  );
+};
+
+export default UserIcon;
---
+++ client/src/components/common/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Logo from './Nav/Logo';
+import Search from './Nav/Search';
+import UserIcon from './Nav/UserIcon';
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="flex justify-between items-center mx-14 h-nav">
+        <Logo />
+        <Search />
+        <UserIcon />
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
