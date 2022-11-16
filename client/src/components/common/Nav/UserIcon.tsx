3277e77c1bb0f365a657ca7279f522068d4edbbb
heoputer <hjw1753@gmail.com>
2022-11-16 22:14:05 +0900

    ✨ feat : 모달 핸들러 추가

--- client/src/components/common/Nav/UserIcon.tsx
+++ client/src/components/common/Nav/UserIcon.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaUserCircle } from 'react-icons/fa';
+import UserIconModal from './UserIconModal';
 
 const UserIcon = () => {
+  const [isModalOpne, setIsModalOpen] = useState(false);
+
+  const ModalHandler = () => {
+    setIsModalOpen(!isModalOpne);
+  };
+
   return (
     <>
-      <div>
-        <FaUserCircle size="50" />
+      <div className="relative">
+        <FaUserCircle size="50" onClick={ModalHandler} />
       </div>
+      {isModalOpne && <UserIconModal />}
     </>
   );
 };
