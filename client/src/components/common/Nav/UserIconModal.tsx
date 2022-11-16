750f4e2c0cde2f6141347cb8492caa6b79f30a6e
heoputer <hjw1753@gmail.com>
2022-11-16 22:14:49 +0900

    ✨feat : 유저 아이콘 모달 (임시)

---
+++ client/src/components/common/Nav/UserIconModal.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UserIconModal = () => {
+  return (
+    <section className="flex flex-col w-36 h-48 bg-point rounded-base absolute top-20 right-10 p-4 text-xl text-white items-center justify-around">
+      <div> 마이페이지 </div>
+      <div> 로그인 </div>
+      <div> 회원가입 </div>
+    </section>
+  );
+};
+
+export default UserIconModal;
