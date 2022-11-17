7bd580327a3be8f5b43a8fab8478437e07e9f302
heoputer <hjw1753@gmail.com>
2022-11-17 15:17:21 +0900

    ✨ feat : Footer 추가 (임시)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/common/Navbar';
+import Footer from './components/common/Footer';
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   );
 }
