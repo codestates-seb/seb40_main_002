f24ce90a103990908e27629933f78f7396bb811d
callmejeje <choije401@naver.com>
2022-11-17 10:06:40 +0900

    💄 style: daisyui themes 추가

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -23,4 +23,22 @@ module.exports = {
     extend: {},
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#6294CD',
+          'primary-content': '#FFFFFF',
+          secondary: '#F000B8',
+          accent: '#37CDBE',
+          neutral: '#3D4451',
+          'base-100': '#FFFFFF',
+          info: '#3ABFF8',
+          success: '#36D399',
+          warning: '#FBBD23',
+          error: '#F87272',
+        },
+      },
+    ],
+  },
 };
