2caff130c1f45ebae3049d440570b4700040da2d
callmejeje <choije401@naver.com>
2022-11-17 22:46:15 +0900

    ✨ feat: 체크박스 컴포넌트 props로 상태 추가

--- client/src/components/common/Checkbox.tsx
+++ client/src/components/common/Checkbox.tsx
@@ -1,8 +1,16 @@
-function Checkbox() {
+function Checkbox({
+  isChecked,
+  setIsChecked,
+}: {
+  isChecked: boolean;
+  setIsChecked: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
   return (
     <input
       type="checkbox"
       className="checkbox checkbox-xs checkbox ml-auto mr-[4px] checkbox-primary"
+      checked={isChecked}
+      onChange={(e) => setIsChecked(e.target.checked)}
     />
   );
 }
