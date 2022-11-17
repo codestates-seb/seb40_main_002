b013a66173fa54c38893909378fd970ffd41d4d1
billy5982 <kuu0404@naver.com>
2022-11-17 17:39:18 +0900

    🐛 fix : App 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import Sample from './components/common/Comment/Sample';
 
 export default function App() {
-  return (
-    <div>
-      <Sample />
-    </div>
-  );
+  return <div>Hello World</div>;
 }
 
 // import React from 'react';
