267f8f642676386a088e2e653c749c1569b90629
heoputer <hjw1753@gmail.com>
2022-11-17 15:19:38 +0900

    💄style : 상단고정, 약간의 스타일 수정

--- client/src/components/common/Navbar.tsx
+++ client/src/components/common/Navbar.tsx
@@ -6,7 +6,7 @@ import UserIcon from './Nav/UserIcon';
 const Navbar = () => {
   return (
     <>
-      <div className="flex justify-between items-center mx-14 h-nav">
+      <div className="flex justify-between items-center w-screen h-[80px] px-10 py-5 fixed top-0">
         <Logo />
         <Search />
         <UserIcon />
