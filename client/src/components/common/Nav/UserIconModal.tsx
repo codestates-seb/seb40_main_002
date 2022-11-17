90440d10a95bf37600af706e92bc468341d30707
heoputer <hjw1753@gmail.com>
2022-11-17 15:27:04 +0900

    💄style : hover 스타일 추가 (임시)

--- client/src/components/common/Nav/UserIconModal.tsx
+++ client/src/components/common/Nav/UserIconModal.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 const UserIconModal = () => {
   return (
-    <section className="flex flex-col w-36 h-48 bg-point rounded-base absolute top-20 right-10 p-4 text-xl text-white items-center justify-around">
-      <div> 마이페이지 </div>
-      <div> 로그인 </div>
-      <div> 회원가입 </div>
+    <section className="flex flex-col w-36 h-40 rounded-[15px] absolute top-[80px] right-10 p-4 text-lg font-semibold items-center justify-around border-solid border-2 border-borderline">
+      <div className="cursor-pointer p-2 hover:bg-point-color hover:text-white rounded-[15px]">
+        마이페이지
+      </div>
+
+      {/* 로그인 상태에 따른 조건부 렌더링 필요 */}
+      <div className="cursor-pointer p-2"> 로그인 </div>
+      <div className="cursor-pointer p-2"> 회원가입 </div>
     </section>
   );
 };
