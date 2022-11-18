37e24f1ff89c48519cb26e31ebf5f3a591c2920a
see1237 <see3722@hanmail.net>
2022-11-18 11:34:02 +0900

    🐛 fix: 예약 엔티티 멤버 수정

--- server/src/main/java/main/project/server/roomreservation/entity/RoomReservation.java
+++ server/src/main/java/main/project/server/roomreservation/entity/RoomReservation.java
@@ -2,6 +2,7 @@ package main.project.server.roomreservation.entity;
 
 import lombok.*;
 import main.project.server.audit.Auditable;
+import main.project.server.member.entity.Member;
 import main.project.server.roomreservation.entity.enums.RoomReservationStatus;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -23,7 +24,7 @@ public class RoomReservation extends Auditable {
 
     @ManyToOne
     @JoinColumn(name = "MEMBER_ID")
-    private String memberId;
+    private Member member;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDateTime roomReservationStart;
