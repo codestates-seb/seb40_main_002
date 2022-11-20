b8fde1059b1133dd0af726fe015ea55c42ea2069
JanuaryKim <unool6976@gmail.com>
2022-11-20 13:23:01 +0900

    ✨ fix: GuestHouseController에 @Validated 추가

--- server/src/main/java/main/project/server/guesthouse/controller/GuestHouseController.java
+++ server/src/main/java/main/project/server/guesthouse/controller/GuestHouseController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -23,7 +24,7 @@ import java.io.IOException;
 import java.security.Principal;
 import java.util.List;
 
-
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class GuestHouseController {
