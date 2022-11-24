8140f0e6c01f6d0e7dfba7f5e68204a5c51dde80
callmejeje <choije401@naver.com>
2022-11-24 14:10:40 +0900

    ✨ feat: 라우팅 추가, 주석 제거

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,23 +3,23 @@ import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
 import { Route, Routes } from 'react-router-dom';
 import PaymentPage from './pages/PaymentPage';
+import MyPage from './pages/MyPage';
+import Main from './pages/Main';
+import SearchResult from './pages/SearchResult';
 
 export default function App() {
   return (
     <div className="w-[100vw] h-[100vh]">
       <Navbar />
-      <div className="flex mx-auto pt-[80px] w-full min-h-[100%] pb-[60px]">
+      <div className="flex mx-auto pt-[80px] w-full min-h-[100%] pb-[60px] h-full max-w-[1120px]">
         <Routes>
           <Route path="/paymentPage" element={<PaymentPage />} />
+          <Route path="/mypage/*" element={<MyPage />} />
+          <Route path="/" element={<Main />} />
+          <Route path="/search" element={<SearchResult />} />
         </Routes>
       </div>
       <Footer />
     </div>
   );
 }
-
-// import React from 'react';
-
-// export default function App() {
-//   return <div className="text-3xl font-bold underline"></div>;
-// }
