fa5735da21c757b2b567dddb4365d26d10522a0f
callmejeje <choije401@naver.com>
2022-11-25 22:53:40 +0900

    ✨ feat: 마이페이지로 이동

--- client/src/components/common/Nav/UserIconModal.tsx
+++ client/src/components/common/Nav/UserIconModal.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const UserIconModal = () => {
   return (
     <section className="flex flex-col w-36 h-40 rounded-[15px] absolute top-[80px] right-10 p-4 text-lg font-semibold items-center justify-around border-solid border-2 border-borderline">
       <div className="cursor-pointer p-2 hover:bg-point-color hover:text-white rounded-[15px]">
-        마이페이지
+        <Link to="/mypage">마이페이지</Link>
       </div>
 
       {/* 로그인 상태에 따른 조건부 렌더링 필요 */}
