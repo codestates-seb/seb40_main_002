24c35bd3c7a27d912e4bd3e6af6b7ee4d042833f
heoputer <hjw1753@gmail.com>
2022-11-28 09:22:56 +0900

    🎨 : input 스타일 변경

--- client/src/components/Register/UserImg.tsx
+++ client/src/components/Register/UserImg.tsx
@@ -1,19 +1,41 @@
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useRef } from 'react';
 
 interface Props {
   userImg: string;
-  setUserImg: Dispatch<SetStateAction<string>>;
+  // setUserImg: Dispatch<SetStateAction<[] | File[]>>;
 }
 
-const UserImg = ({ userImg, setUserImg }: Props) => {
+const UserImg = ({ userImg }: Props) => {
+  const imgRef = useRef<HTMLInputElement>(null);
+  const imgRefClick = () => {
+    if (imgRef.current) {
+      imgRef.current.click();
+    }
+  };
+
+  const selectImg = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedfiles = e.target.files;
+    if (selectedfiles) {
+      const uploadfiles = Array.from(selectedfiles);
+    }
+    // setUserImg(selectedfiles);
+  };
+
   return (
     <div className="flex flex-col justify-center items-center">
       <img
         className="w-[242px] h-[242px] rounded-full hover:opacity-80 transition-all duration-150 mb-[30px]"
         alt="userPic"
         src={userImg}
+        onClick={imgRefClick}
       />
-      <input type={'file'} accept="image/*" />
+      <input
+        type={'file'}
+        accept="image/*"
+        className="hidden"
+        ref={imgRef}
+        onChange={selectImg}
+      />
     </div>
   );
 };
