23e8ed45273fe8455a1418542883c443d9a77fe6
see1237 <see3722@hanmail.net>
2022-11-29 23:54:14 +0900

    🚚 JwtTokenizer 위치 변경

--- server/src/main/java/main/project/server/member/dto/MemberDto.java
+++ server/src/main/java/main/project/server/member/dto/MemberDto.java
@@ -3,11 +3,12 @@ package main.project.server.member.dto;
 import lombok.*;
 import main.project.server.member.entity.enums.MemberNationality;
 import main.project.server.member.entity.enums.MemberRegisterKind;
-import main.project.server.validation.ValidRole;
+import main.project.server.validation.role.ValidRole;
 
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.List;
 
 public class MemberDto {
@@ -26,11 +27,14 @@ public class MemberDto {
         @Email
         @NotBlank
         private String memberEmail;
-        @NotBlank
+        @Pattern(regexp = "^010-\\d{3,4}-\\d{4}$",
+                message = "010으로 시작하고 숫자와 '-'로 구성된 휴대폰 번호 형식이어야 합니다")
         private String memberPhone;
 
 //        private MemberStatus memberStatus;
 
+        @Pattern(regexp = "^([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))$",
+                message = "생년월일은 유효한 'yyyy-mm-dd' 형식이어야 합니다.")
         private String memberBirth;
 
         private MemberNationality memberNationality;
