fcc1b667211abd92bbafbb9a602b84176811571c
JanuaryKim <unool6976@gmail.com>
2022-11-29 20:03:06 +0900

    feat: querydsl 테스트 추가

---
+++ server/src/test/java/main/project/server/querydsl/QueryDslTests.java
@@ -0,0 +1,24 @@
+package main.project.server.querydsl;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+@SpringBootTest
+@Transactional
+public class QueryDslTests {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void basicTest() {
+
+
+
+    }
+}
