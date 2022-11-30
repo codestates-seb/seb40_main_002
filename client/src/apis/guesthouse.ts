51e98d56b1f5c6f90ca250e93c18e889355d3c34
callmejeje <choije401@naver.com>
2022-11-30 22:24:30 +0900

    ♻️ refactor: 주석 제거, setTotalCount props에 없을 때 해결

--- client/src/apis/guesthouse.ts
+++ client/src/apis/guesthouse.ts
@@ -2,17 +2,17 @@ import axios, { AxiosResponse } from 'axios';
 import React from 'react';
 import { GuestHouseShort, RoomShort } from '../types/guesthouse';
 
-const API = 'http://3.37.58.81:8080';
-
 export const getGuesthouseList = async (
   path: string,
-  setTotalCount: React.Dispatch<React.SetStateAction<number>>
+  setTotalCount?: React.Dispatch<React.SetStateAction<number>>
 ) => {
   const data = await axios.get(`${path}`).then((res: AxiosResponse) => {
     // console.log(res.data);
     const totalCount = res.data.pageInfo.totalElements;
-    if (totalCount) setTotalCount(totalCount);
-    else setTotalCount(0);
+    if (setTotalCount) {
+      if (totalCount) setTotalCount(totalCount);
+      else setTotalCount(0);
+    }
     const newGuesthouses: GuestHouseShort[] = res.data.data.map(
       (el: {
         guestHouseImage: string[];
@@ -24,9 +24,7 @@ export const getGuesthouseList = async (
       }) => {
         const price = Math.min(...el.rooms.map((room) => room.roomPrice));
         const guesthouse: GuestHouseShort = {
-          imgSrc: `${API}${el.guestHouseImage[0]}`,
-          // imgSrc:
-          // 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/PhodopusSungorus_2.jpg/640px-PhodopusSungorus_2.jpg',
+          imgSrc: `${process.env.REACT_APP_SERVER_URL}${el.guestHouseImage[0]}`,
           name: el.guestHouseName,
           price: price,
           star: el.guestHouseStar,
