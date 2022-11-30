7fef437e1cb3f11d10f766ab7d2bd118010f72eb
JanuaryKim <unool6976@gmail.com>
2022-12-01 06:14:46 +0900

    fix: start 쉘 변경

--- server/scripts/server_start.sh
+++ server/scripts/server_start.sh
@@ -4,6 +4,5 @@
 # 2 : nohup - 해당 명령어를 입력하는 터미널의 연결이 끊켜도 프로세스를 실행시킴.
 # 2 : > /dev/null 2> - 해당 명령어로 인해 발생되는 출력내용(에러 포함)을 버림
 cd /home/ubuntu/build
-#nohup java -jar server-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
-#sudo nohup java -jar server-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev > /dev/null 2> /dev/null < /dev/null &
-sudo nohup java -jar server-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+sudo nohup java -jar -Dspring.profiles.active=dev server-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+
