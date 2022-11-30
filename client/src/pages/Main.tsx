ecb4733a80052453017b8d070931ae9363508728
callmejeje <choije401@naver.com>
2022-11-30 22:26:42 +0900

    ✨ feat: 유저 정보(태그)에 따라 추천 리스트 요청 추가

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,34 +1,38 @@
 // import React, { useCallback, useEffect, useState } from 'react';
 // import { useInView } from 'react-intersection-observer';
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import GuesthouseList from '../components/Main/GuesthouseList';
 import useInfiniteScroll from '../hooks/useInfiniteScroll';
 import { GuestHouseShort } from '../types/guesthouse';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/store';
+import { isLogin } from '../utils/isLogin';
+import { getGuesthouseList } from '../apis/guesthouse';
 
 function Main() {
   const mainUser = useSelector((state: RootState) => state.user);
-  // 임시 데이터
-  const testGh: GuestHouseShort = {
-    imgSrc:
-      'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80',
-    name: '정우네 게스트 하우스',
-    price: 105000,
-    star: 4.5,
-    tags: ['오션뷰', '대리석', '시골'],
-    id: 1,
-  };
-  const guesthouses: Array<GuestHouseShort> = new Array(100).fill(testGh);
   const [list, setSortType, ref] = useInfiniteScroll('/api/all-guesthouse');
+  const [myRecommended, setMyRecommended] = useState<GuestHouseShort[]>([]);
+
+  const getRecommended = useCallback(async () => {
+    const myTags = mainUser.memberTag.join('&tag=');
+    const path = `/api/all-guesthouse?page=1&size=10&tag=${myTags}&sort=default`;
+    // const path = `/api/all-guesthouse?page=1&size=10&tag=${'오션뷰'}&sort=default`;
+
+    console.log(path);
+    const recommended = await getGuesthouseList(path);
+    setMyRecommended(recommended);
+  }, [mainUser.memberTag]);
+
   useEffect(() => {
-    // console.log('Main User: ', mainUser);
-    // 추천 게스트하우스 지정
-  }, [mainUser]);
+    getRecommended();
+  }, [mainUser.memberTag]);
 
   return (
     <div className="w-full p-[20px] h-full">
-      <GuesthouseList header={'추천'} guesthouses={guesthouses.slice(0, 8)} />
+      {isLogin() && (
+        <GuesthouseList header={'추천'} guesthouses={myRecommended} />
+      )}
       <GuesthouseList
         header={'일반'}
         guesthouses={list}
