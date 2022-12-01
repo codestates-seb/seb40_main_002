54d81f875ec600e1ee65be8403e738a4ffdfe687
heoputer <hjw1753@gmail.com>
2022-12-01 15:24:17 +0900

    🐛 fix : api url fix

--- client/src/apis/getReviewData.ts
+++ client/src/apis/getReviewData.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const getReviewData = async (ghId: string | undefined) => {
   try {
     const response = await axios.get(
-      `http://3.37.58.81:8080/api/guesthouse/${ghId}/review?page=1&size=10`
+      `/api/guesthouse/${ghId}/review?page=1&size=10`
     );
     return response.data.data;
   } catch (err) {
