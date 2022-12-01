87563bcca6a8ef7203a4183cfe87c62bb6b86c20
JanuaryKim <unool6976@gmail.com>
2022-12-02 00:04:52 +0900

    ♻refactor : 부분 리팩토링3

---
+++ server/src/main/java/main/project/server/annotation/validation/enumvalid/EnumValidator.java
@@ -0,0 +1,27 @@
+package main.project.server.annotation.validation.enumvalid;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class EnumValidator implements ConstraintValidator<ValidEnum, Enum> {
+
+    private ValidEnum annotation;
+
+    @Override
+    public void initialize(ValidEnum constraintAnnotation) {
+        this.annotation = constraintAnnotation;
+    }
+
+    @Override
+    public boolean isValid(Enum value, ConstraintValidatorContext context) {
+        Object[] enumValues = this.annotation.enumClass().getEnumConstants();
+        if (enumValues != null) {
+            for (Object enumValue : enumValues) {
+                if (value == enumValue) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
--- server/src/main/java/main/project/server/statistics/controller/StatisticsController.java
+++ server/src/main/java/main/project/server/statistics/controller/StatisticsController.java
@@ -2,16 +2,18 @@ package main.project.server.statistics.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import main.project.server.dto.SingleResponseDto;
+import main.project.server.statistics.condition.SearchCondition;
+import main.project.server.statistics.dto.AgeChartDto;
+import main.project.server.statistics.dto.MonthlyReservationChartDto;
 import main.project.server.statistics.dto.ReserveCountChartDto;
 import main.project.server.statistics.dto.ReserveCountOfGuestHouseDto;
 import main.project.server.statistics.service.StatisticsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -29,5 +31,22 @@ public class StatisticsController {
         ReserveCountChartDto reserveCountChartDto = ReserveCountChartDto.of(periodChart);
 
         return new ResponseEntity(reserveCountChartDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/api/auth/monthly-chart/{guest-house-id}")
+    public ResponseEntity monthlyChart(@PathVariable("guest-house-id") Long guestHouseId,
+                                       @RequestParam Integer year,
+                                       Principal principal) {
+        MonthlyReservationChartDto result = service.getMonthlyReservationChart(guestHouseId, year, principal);
+        return new ResponseEntity<>(new SingleResponseDto<>("get", result), HttpStatus.OK);
+    }
+
+    // 연령별 예약 통계 조답
+    @GetMapping("/api/auth/age-chart/{guest-house-id}")
+    public ResponseEntity ageChart(@PathVariable("guest-house-id") Long guestHouseId,
+                                   @ModelAttribute SearchCondition condition,
+                                   Principal principal) {
+        AgeChartDto result = service.getAgeChart(guestHouseId, condition, principal);
+        return new ResponseEntity<>(new SingleResponseDto<>("get", result), HttpStatus.OK);
     }
 }
