834a47427e0373a9249fa69e9d19c8ab4f7a5cd6
billy5982 <kuu0404@naver.com>
2022-12-03 04:16:32 +0900

    구글 이미지 테스트 사진 타입 강제 변경

--- client/src/libs/srcToFile.ts
+++ client/src/libs/srcToFile.ts
@@ -12,6 +12,6 @@ export const convertURLtoFileGoogle = async (url: string) => {
   const data = await response.blob();
   const ext = url.split('.').pop(); // url 구조에 맞게 수정할 것
   const filename = url.split('/').pop(); // url 구조에 맞게 수정할 것
-  const metadata = { type: `image/png` };
+  const metadata = { type: `image/jpeg` };
   return new File([data], filename!, metadata);
 };
