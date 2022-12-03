4bd745993663ab7f0c9d15bda1cc878bc3893d11
billy5982 <kuu0404@naver.com>
2022-12-03 18:13:23 +0900

    인터셉터 테스트

--- client/src/api2/index.ts
+++ client/src/api2/index.ts
@@ -42,8 +42,9 @@ Api.interceptors.response.use(
         });
 
         if (data) {
-          const newAccessToken = data.data.data.accessToken;
-          localStorage.setItem('accessToken', newAccessToken);
+          const newAccessToken = JSON.parse(data.data.data.accessToken);
+
+          localStorage.setItem('accessToken', JSON.stringify(newAccessToken));
         }
       } catch (err) {
         console.log('토큰 인증 오류 발생');
