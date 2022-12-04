3ccd3634e38ea0b9ad6f26004e985f0a1e582429
see1237 <see3722@hanmail.net>
2022-12-04 17:13:27 +0900

    ♻️ refactor: refresh token 요청, 응답 방식 변경(헤더)

--- server/src/main/java/main/project/server/security/jwt/controller/TokenController.java
+++ server/src/main/java/main/project/server/security/jwt/controller/TokenController.java
@@ -1,9 +1,12 @@
 package main.project.server.security.jwt.controller;
 
+import com.nimbusds.openid.connect.sdk.AuthenticationResponse;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import main.project.server.dto.SingleResponseDto;
+import main.project.server.exception.AuthException;
+import main.project.server.exception.ExceptionCode;
 import main.project.server.security.jwt.service.JwtTokenizer;
 import main.project.server.security.jwt.dto.TokenDto;
 import main.project.server.security.jwt.entity.RefreshToken;
@@ -13,13 +16,17 @@ import main.project.server.member.service.MemberService;
 import main.project.server.security.jwt.service.TokenService;
 import main.project.server.security.oauth.handler.OauthSuccessHandler;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @RestController
@@ -45,22 +52,24 @@ public class TokenController {
 
     // refreshToken을 이용해 새로운 accessToken 발급 요청
     @PostMapping("/api/token")
-    public ResponseEntity getAccessToken(@RequestBody @Valid TokenDto tokenDto) {
+    public ResponseEntity<Object> getAccessToken(HttpServletRequest request, HttpServletResponse response) {
 
 
-        RefreshToken refreshToken = tokenMapper.tokenPostToRefreshToken(tokenDto);
+//        RefreshToken refreshToken = tokenMapper.tokenPostToRefreshToken(tokenDto);
+        String token = request.getHeader("RefreshToken");
 
         String encodedBase64SecretKey = jwtTokenizer.encodeBase64SecretKey(secretKey);
 
         // refresh 토큰 검사, memberId 추출
-        String memberIdFromToken = jwtTokenizer.getMemberIdFromToken(refreshToken.getRefreshToken(), encodedBase64SecretKey);
-        tokenService.findVerifiedToken(memberIdFromToken);
+        String memberIdFromToken = jwtTokenizer.getMemberIdFromToken(token, encodedBase64SecretKey);
 
+        tokenService.verifyToken(memberIdFromToken, token);
         Member member = memberService.findVerifiedMember(memberIdFromToken);
         String accessToken = oauthSuccessHandler.delegateAccessToken(member);
 
         log.info("accessToken = {}", accessToken);
+        response.addHeader("Authorization", accessToken);
 
-        return new ResponseEntity(accessToken, HttpStatus.OK);
+        return new ResponseEntity("created", HttpStatus.OK);
     }
 }
