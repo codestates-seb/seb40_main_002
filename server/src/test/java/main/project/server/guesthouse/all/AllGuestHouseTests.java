a930d22ecbc325f52791d93efff5cb7665a15f2c
JanuaryKim <unool6976@gmail.com>
2022-12-06 03:13:16 +0900

    feat: gitignore에 테스트용 yml 제외시킴

--- server/src/test/java/main/project/server/guesthouse/all/AllGuestHouseTests.java
+++ server/src/test/java/main/project/server/guesthouse/all/AllGuestHouseTests.java
@@ -1,10 +1,28 @@
 package main.project.server.guesthouse.all;
 
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import javax.transaction.Transactional;
+
+@Transactional
 @SpringBootTest
 @ActiveProfiles("test")
 public class AllGuestHouseTests {
+
+
+
+    @DisplayName("모든 게스트하우스 페이지네이션 - 모든 파라미터 X")
+    @Test
+    void allParametersNoting() {
+
+        
+
+
+    }
+
+
 }
