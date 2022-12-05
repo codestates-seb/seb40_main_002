8b5226672cd6b896604d4f2915782f843ccdebf9
billy5982 <kuu0404@naver.com>
2022-12-06 03:32:26 +0900

    refactoring : axios to Api

--- client/src/apis/getHeartData.ts
+++ client/src/apis/getHeartData.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
+import Api from '../api2';
 
 export const getHeartData = async (url: string) => {
   const accessToken = localStorage.getItem('accessToken');
 
   try {
-    const response = await axios.get(`/api/auth/members/heart${url}`, {
+    const response = await Api.get(`/api/auth/members/heart${url}`, {
       headers: {
-        Authorization: accessToken,
+        // Authorization: accessToken,
       },
     });
     const data = await response.data;
--- client/src/apis/getUserInfo.ts
+++ client/src/apis/getUserInfo.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import Api from '../api2';
 
 export const getUserInfo = async (token: string | null) => {
   try {
-    const response = await axios.get(`/api/auth/members`, {
+    const response = await Api.get(`/api/auth/members`, {
       headers: { Authorization: `${token}` },
     });
     return response.data.data;
