f100641ccd25a23b5d67150605f74803721acaf2
billy5982 <kuu0404@naver.com>
2022-12-06 13:17:19 +0900

    style : footer z-index 추가

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -33,28 +33,26 @@ const Footer = () => {
   ];
 
   return (
-    <>
-      <div className="h-[60px] w-full flex justify-between items-center z-[100] fixed bottom-0 bg-footer border-2 border-black-100">
-        <p className="font-semibold left-6 text-footerFont ml-6">
-          팀 2rror, 빛과 소금...그리고 우리
-        </p>
-        <div className="flex">
-          {memberList.map((member, idx) => {
-            return (
-              <div key={idx} className="flex mr-6">
-                <a
-                  href={member.memberLink}
-                  className="mr-2 flex items-center text-footerFont"
-                >
-                  <AiOutlineGithub />
-                  {member.memberName}
-                </a>
-              </div>
-            );
-          })}
-        </div>
+    <div className="h-[60px] w-full flex justify-between items-center z-[100] fixed bottom-0 bg-footer border-2 border-black-100">
+      <p className="font-semibold left-6 text-footerFont ml-6">
+        팀 2rror, 빛과 소금...그리고 우리
+      </p>
+      <div className="flex">
+        {memberList.map((member, idx) => {
+          return (
+            <div key={idx} className="flex mr-6">
+              <a
+                href={member.memberLink}
+                className="mr-2 flex items-center text-footerFont"
+              >
+                <AiOutlineGithub />
+                {member.memberName}
+              </a>
+            </div>
+          );
+        })}
       </div>
-    </>
+    </div>
   );
 };
 
